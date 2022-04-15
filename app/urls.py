e22697e7cf65f83c0ca245669f55f443d4efa09a
ookuboc5399 <ankle0614@gmail.com>
2022-04-15 12:45:28 +0900

    0415

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from .views import VideoViewSet, CreateUserView
+
+
+router = routers.DefaultRouter()
+router.register('videos', VideoViewSet)
+
+urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create'),
+    path('',include(router.urls)),
+]
